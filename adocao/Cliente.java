d2fa4aebe6e6d1190db58f35332902c0275195d1
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-05-26 11:45:30 -0300

    Estrutura de adoção

---
+++ adocao/Cliente.java
@@ -0,0 +1,17 @@
+package adocao;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private int idade;
+    private char sexo;
+
+    public Cliente(String nome, String cpf, String endereco, int idade, char sexo) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+}
