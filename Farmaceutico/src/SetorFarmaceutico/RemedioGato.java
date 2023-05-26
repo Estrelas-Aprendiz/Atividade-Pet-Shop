005b3144945da1dd3bf79e4d2517ec50f73070d7
Raissa Vicente <raissa.carvalho@zup.com.br>
2023-05-26 13:59:51 -0300

    Atualizado

--- Farmaceutico/src/SetorFarmaceutico/RemedioGato.java
+++ Farmaceutico/src/SetorFarmaceutico/RemedioGato.java
@@ -1,7 +1,12 @@
 package SetorFarmaceutico;
 
 public class RemedioGato extends Remedio implements Farmacia{
-    public RemedioGato(String nome, double miligrama) {
-        super(nome, miligrama);
+
+    public RemedioGato(String nome, double miligrama, double preco) {
+        super();
+    }
+
+    @Override
+    public void vender() {
     }
 }
