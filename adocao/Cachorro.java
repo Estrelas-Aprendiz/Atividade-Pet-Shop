eeebbe096073ae26a86611c7a0982bf8bc7ae846
Carolina De Carvalho Queiroz <88510089+carolina-queiroz@users.noreply.github.com>
2023-05-26 11:46:02 -0300

    Delete adocao directory

--- adocao/Cachorro.java
+++
@@ -1,7 +0,0 @@
-package adocao;
-
-public class Cachorro extends Animal{
-    public Cachorro(String raca, String nome, int idade, double peso) {
-        super(raca, nome, idade, peso);
-    }
-}
--- adocao/Gato.java
+++
@@ -1,7 +0,0 @@
-package adocao;
-
-public class Gato extends Animal{
-    public Gato(String raca, String nome, int idade, double peso) {
-        super(raca, nome, idade, peso);
-    }
-}
